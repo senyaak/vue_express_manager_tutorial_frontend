015e18cc372b0ab6ff9b78b04ecfa85f7ea38dc4
Arsenij Schützer <aschuetzer@saltation.de>
2017-12-22 14:52:28 +0100

    correct css import

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ Vue.use(VueCookie);
 import Authentication from "./components/pages/Authentication";
 import App from "./components/App";
 import Router from "./router/index";
-import("./../node_modules/vuetify/dist/vuetify.min.css");
+import "./../node_modules/vuetify/dist/vuetify.min.css";
 Vue.config.productionTip = false;
 
 // check auth on load (without this we should login every time page loads)
